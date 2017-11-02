0c89cd55bfeb3e3d51560dd59da3dc6818fd5378
Unknown <lama.kevin50@gmail.com>
2017-11-02 21:18:54 +0100

    backgroung menu

--- js/states/menu.js
+++ js/states/menu.js
@@ -2,10 +2,12 @@ let menuState = {
 
 
     create: function () {
-
+    	game.stage.backgroundColor = "#4488AA";
         game.add.text(20,150,'menu',{fill:'#fff'});
 
     	buttonPlay = game.add.button(game.world.centerX - 95,300,'button-green',this.go,this,2,1,0);
+
+        game.add.text(610,307,'Play');
     },
 
     go: function () {
