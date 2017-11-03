1fc5e5ab227e5379735cc1eb66f336069e88d439
Unknown <lama.kevin50@gmail.com>
2017-11-03 21:23:12 +0100

    Auto stash before merge of "master" and "origin/master"

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ game.global = {
 };
 
 window.game = game;
+game.state.add('card',cardState);
 game.state.add('init',initState);
 game.state.add('menu',menuState);
 game.state.add('play',playState);
--- js/states/Menu.js
+++ js/states/Menu.js
@@ -5,13 +5,15 @@ let menuState = {
     	game.add.sprite(0,0, 'bg');
 
     	buttonCredits = game.add.button(game.world.centerX +15,10,'button-credits',this.cr,this,2,1,0);
-    	buttonPlay = game.add.button(game.world.centerX -165,10,'button-play',this.go,this,2,1,0);
+    	buttonPlay = game.add.button(game.world.centerX -165,10,'button-play',this.ca,this,2,1,0);
 
-        let text = game.add.text(100, 100, "Json test: " + cards.cards[0].name, { fill: '#ffffff' });
+        let text = game.add.text(100, 100, "Json test: " + cards.cards[18].name, { fill: '#ffffff' });
+
+        
     },
 
-    go: function () {
-        game.state.start("play")
+    ca: function () {
+        game.state.start("card")
     },
     cr: function () {
         game.state.start("credits")
