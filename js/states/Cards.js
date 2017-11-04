198c5b2850edc5e142026efbf34484969b46adc0
Unknown <kubamoo@o2.pl>
2017-11-04 16:09:19 +0100

    nie działa konec czasu

--- js/states/Cards.js
+++ js/states/Cards.js
@@ -2,8 +2,8 @@ let cardState = {
     
     create: function () {
 
-        buttonPlay = game.add.button(game.world.centerX -165,10,'button-play',this.go,this,2,1,0);
-        buttonCard = game.add.button(15,100,'kartap',this.go,this,2,1,0);
+        let buttonPlay = game.add.button(game.world.centerX -165,10,'button-play',this.go,this,2,1,0);
+        let buttonCard = game.add.button(15,100,'kartap',this.go,this,2,1,0);
         
     },
     
