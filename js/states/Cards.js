71c3f91342c0733d40d5561f647687f75d1bf115
Unknown <lama.kevin50@gmail.com>
2017-11-04 18:33:33 +0100

    Tablica

--- js/states/Cards.js
+++ js/states/Cards.js
@@ -1,8 +1,6 @@
 let cardState = {
     
     create: function () {
-
-<<<<<<< HEAD
         buttonPlay = game.add.button(game.world.centerX -165,10,'button-play',this.go,this,2,1,0);
         let sel = game.add.text(game.world.centerX, 30, "Selected:", {fill: 'red'});
         buttonCard = game.add.button(15,100,'kartap',this.go1,this,2,1,0);
@@ -35,11 +33,21 @@ let cardState = {
         let name13 = game.add.text(955, 395, cards.cards[13].name, {fill: 'black' , fontSize: '15px'});
         let name14 = game.add.text(1135, 395, cards.cards[14].name, {fill: 'black' , fontSize: '15px'});
 
-=======
-        let buttonPlay = game.add.button(game.world.centerX -165,10,'button-play',this.go,this,2,1,0);
-        let buttonCard = game.add.button(15,100,'kartap',this.go,this,2,1,0);
-        
->>>>>>> master
+
+        let description1 = game.add.text(40, 280, cards.cards[1].description, {fill: 'black' , fontSize: '8px'});
+        let description2 = game.add.text(230, 280, cards.cards[2].description, {fill: 'black' , fontSize: '8px'});
+        let description3 = game.add.text(410, 280, cards.cards[3].description, {fill: 'black' , fontSize: '8px'});
+        let description4 = game.add.text(570, 280, cards.cards[4].description, {fill: 'black' , fontSize: '8px'});
+        let description5 = game.add.text(760, 280, cards.cards[5].description, {fill: 'black' , fontSize: '8px'});
+        let description6 = game.add.text(940, 280, cards.cards[6].description, {fill: 'black' , fontSize: '8px'});
+        let description7 = game.add.text(1110, 280, cards.cards[7].description, {fill: 'black' , fontSize: '8px'});
+        let description8 = game.add.text(40, 555, cards.cards[8].description, {fill: 'black' , fontSize: '8px'});
+        let description9 = game.add.text(220, 555, cards.cards[9].description, {fill: 'black' , fontSize: '8px'});
+        let description10 = game.add.text(410, 555, cards.cards[10].description, {fill: 'black' , fontSize: '8px'});
+        let description11 = game.add.text(570, 555, cards.cards[11].description, {fill: 'black' , fontSize: '8px'});
+        let description12 = game.add.text(760, 555, cards.cards[12].description, {fill: 'black' , fontSize: '8px'});
+        let description13 = game.add.text(940, 555, cards.cards[13].description, {fill: 'black' , fontSize: '8px'});
+        let description14 = game.add.text(1110, 555, cards.cards[14].description, {fill: 'black' , fontSize: '8px'});
     },
     
     update: function () {
